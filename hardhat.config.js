f2a5f68e31887b473f50fd7b1f1a418d1fe9fad7
aa-wong <waaron.wong@gmail.com>
2022-10-08 19:31:26 -0400

    smart contract dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { ALCHEMY_API_KEY, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/" + ALCHEMY_API_KEY,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
