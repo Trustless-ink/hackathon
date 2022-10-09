f7d4d1bbda7091b7a1948b9cb73d3bdfaa5a329c
aa-wong <waaron.wong@gmail.com>
2022-10-08 23:26:59 -0400

    Create function with modifiers

--- contracts/Trustless.sol
+++ contracts/Trustless.sol
@@ -31,6 +31,26 @@ contract Trustless is ERC1155URIStorage {
     uint availableToWithdraw;
   }
 
+  /**
+   * Modifiers
+   */
+  modifier stillFundraising(uint tokenId) {
+    Project memory project = projects[tokenId];
+    require(project.fundraising && project.active, "Fundraising campaign is closed");
+    require(project.balance < project.goal, "Campaign is inactive.");
+    _;
+  }
+
+  modifier onlyFundraiser(uint tokenId) {
+    require(msg.sender == projects[tokenId].fundraiser, "Only the fundraiser can execute this method.");
+    _;
+  }
+
+  modifier onlyContributors(uint tokenId) {
+    require(balanceOf(msg.sender, tokenId) > 0, "Only the contributors can execute this method.");
+    _;
+  }
+
   constructor() ERC1155("") {}
 
   /**
@@ -44,7 +64,7 @@ contract Trustless is ERC1155URIStorage {
     return projects[tokenId].balance;
   }
 
-  function availableToWithdraw(uint tokenId) external view returns (uint) {
+  function availableToWithdraw(uint tokenId) external view onlyFundraiser(tokenId) returns (uint) {
     return projects[tokenId].availableToWithdraw;
   }
 
@@ -66,6 +86,38 @@ contract Trustless is ERC1155URIStorage {
 
   function isActive(uint tokenId) external view returns (bool) {
     return projects[tokenId].active;
+  }
+
+  /**
+   * Methods
+   */
+  function create(
+    uint _goal,
+    string memory _tokenURI,
+    uint _intervalTime, // In minutes
+    uint _milestones
+  ) external returns (uint) {
+    require(_goal > 0, "Fundraising goal must be greater than 0");
+
+    projectCount++;
+    Project memory newProject = Project({
+      id: projectCount,
+      intervalTime: _intervalTime,
+      timestamp: block.timestamp,
+      milestones: _milestones,
+      goal: _goal,
+      balance: 0,
+      fundraiser: msg.sender,
+      active: true,
+      fundraising: true,
+      terminated: false,
+      availableToWithdraw: 0
+    });
+
+    _setURI(projectCount, _tokenURI);
+    projects[projectCount] = newProject;
+    emit ProjectCreated(msg.sender, projectCount, _goal);
+    return projectCount;
   }
   /**
    * Receive function issue the basic token if msg.sender is paying directly
