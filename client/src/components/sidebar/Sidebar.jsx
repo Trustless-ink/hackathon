3adacbcfcc06a8941fe2e5580bc544aeaf6f7e57
Noe Horowitz <noehoro@gmail.com>
2022-10-08 20:22:52 -0500

    metamask integration

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -3,9 +3,44 @@ import DashboardIcon from '@mui/icons-material/Dashboard'
 import StoreIcon from '@mui/icons-material/Store'
 import CreditCardIcon from '@mui/icons-material/CreditCard'
 import InputOutlinedIcon from '@mui/icons-material/InputOutlined'
+
 import { Link } from 'react-router-dom'
+import Context from '../../helpers/Context'
+import getWeb3 from '../../helpers/getWeb3'
+import { useContext } from 'react'
 
 const Sidebar = () => {
+  const { contextValue, dispatchContextValue } = useContext(Context)
+
+  const handleLogin = async () => {
+    if (contextValue.loggedIn) {
+      dispatchContextValue({
+        type: 'logout',
+      })
+      return
+    }
+    try {
+      // Get network provider (typically MetaMask) and web3 instance
+      const web3 = await getWeb3()
+
+      // Use web3 to get the user's accounts from the provider (MetaMask)
+      const accounts = await web3.eth.getAccounts()
+
+      const networkId = await web3.eth.net.getId()
+
+      dispatchContextValue({
+        type: 'login',
+        payload: { accounts, networkId },
+      })
+    } catch (error) {
+      // Catch any errors for any of the above operations
+      alert(
+        `Failed to load web3, accounts, or contract. Did you migrate the contract or install MetaMask? Check console for details.`,
+      )
+      console.error(error)
+    }
+  }
+
   return (
     <div className="sidebar">
       <div className="top">
@@ -37,10 +72,10 @@ const Sidebar = () => {
           </Link>
         </ul>
       </div>
-      <div className="bottom">
+      <button className="bottom" onClick={handleLogin}>
         <InputOutlinedIcon className="icon" />
-        <span>Logout</span>
-      </div>
+        <span>{contextValue.loggedIn ? 'Logout' : 'login'}</span>
+      </button>
     </div>
   )
 }
