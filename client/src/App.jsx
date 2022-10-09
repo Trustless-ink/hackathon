4e031558c70558bb6a5578e8214bf5d3462344d1
Noe Horowitz <noehoro@gmail.com>
2022-10-08 20:22:38 -0500

    change app.js to app.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useReducer } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/home/Home'
+import NewProject from './pages/NewProject'
+import FundProject from './pages/FundProject'
+import './app.scss'
+
+import { contextReducer } from './helpers/reducers'
+import Context from './helpers/Context'
+import { getLocalStorageObject } from './helpers/helperFunctions'
+
+function App() {
+  const [contextValue, dispatchContextValue] = useReducer(contextReducer, {
+    loggedIn: localStorage.getItem('loggedIn'),
+    web3: getLocalStorageObject('web3'),
+  })
+  return (
+    <Context.Provider value={{ contextValue, dispatchContextValue }}>
+      <div className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/newproject" element={<NewProject />} />
+            <Route path="/fundproject" element={<FundProject />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Context.Provider>
+  )
+}
+
+export default App
